e4770af81f0ca683d7438a2e515f808417f9ee6f
Romain Bodard <romain.bodard@videotron.ca>
2025-09-23 10:15:34 -0400

    Added Localization support on the FormattedBindingListener

--- Runtime/Binding/FormattedBindingListener.cs
+++ Runtime/Binding/FormattedBindingListener.cs
@@ -2,18 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+#if UNITY_LOCALIZATION
+using UnityEngine.Localization;
+#endif
+
 namespace Bodardr.Databinding.Runtime
 {
     public class FormattedBindingListener : BindingListener
     {
         private TypeCode getterTypeCode;
+        private object[] args = new object[1];
 
         [Tooltip("These getters start from index {1} inside the string format, onward")]
         [SerializeField]
         private List<BindingGetExpression> additionalGetters = new();
 
+        #if UNITY_LOCALIZATION
+        [SerializeField]
+        private bool localize;
+
+        [SerializeField]
+        [ShowIf(nameof(localize))]
+        private LocalizedString localizedString;
+        #endif
+
+        #if UNITY_LOCALIZATION
+        [ShowIf(nameof(localize), true)]
+        #endif
         [SerializeField]
         private string format;
+
 
         [HideInInspector]
         [SerializeField]
@@ -24,7 +42,7 @@ namespace Bodardr.Databinding.Runtime
         private bool convertGetterToTimeSpan;
 
 #if !ENABLE_IL2CPP || UNITY_EDITOR
-        public override void  QueryExpressions(
+        public override void QueryExpressions(
             Dictionary<Type, Dictionary<string, Tuple<IBindingExpression, GameObject>>> expressions,
             bool fromAoT)
         {
@@ -69,6 +87,11 @@ namespace Bodardr.Databinding.Runtime
                 getter.Subscribe(this, bindingNode);
 
             base.OnEnable();
+
+            #if UNITY_LOCALIZATION
+            if (localize && localizedString != null)
+                localizedString.StringChanged += BindingUpdatedFromLocalization;
+            #endif
         }
 
         protected override void OnDisable()
@@ -77,6 +100,16 @@ namespace Bodardr.Databinding.Runtime
 
             foreach (var getter in additionalGetters)
                 getter.Unsubscribe(this, bindingNode);
+
+            #if UNITY_LOCALIZATION
+            if (localize && localizedString != null)
+                localizedString.StringChanged -= BindingUpdatedFromLocalization;
+            #endif
+        }
+
+        private void BindingUpdatedFromLocalization(string localizedString)
+        {
+            OnBindingUpdated(bindingNode != null ? bindingNode.Binding : null);
         }
 
         public override void OnBindingUpdated(object obj)
@@ -85,24 +118,33 @@ namespace Bodardr.Databinding.Runtime
 
             var go = gameObject;
 
+            //First argument.
             var fetchedValue = GetExpression.Invoke(obj, go);
 
             if (fetchedValue != null && getterExpressionIsNumeric && convertGetterToTimeSpan)
                 fetchedValue = TimeSpan.FromSeconds(UnboxValueToDouble(fetchedValue, getterTypeCode));
 
+            args[0] = fetchedValue;
+
+
             if (additionalGetters.Count > 0)
             {
-                object[] values = new object[additionalGetters.Count + 1];
-                values[0] = fetchedValue;
-                for (var i = 0; i < additionalGetters.Count; i++)
-                    values[i + 1] = additionalGetters[i].Invoke(obj, go);
+                if (args.Length != additionalGetters.Count + 1)
+                    args = new object[additionalGetters.Count + 1];
 
-                SetExpression.Invoke(obj, string.Format(format, values), go);
+                args[0] = fetchedValue;
+                for (var i = 0; i < additionalGetters.Count; i++)
+                    args[i + 1] = additionalGetters[i].Invoke(obj, go);
             }
+
+#if UNITY_LOCALIZATION
+            if (localize)
+                SetExpression.Invoke(obj, localizedString.GetLocalizedString(args), go);
             else
-            {
-                SetExpression.Invoke(obj, string.Format(format, fetchedValue ?? string.Empty), go);
-            }
+                SetExpression.Invoke(obj, string.Format(format, args), go);
+#else            
+            SetExpression.Invoke(obj, string.Format(format, args), go);
+#endif
         }
 
         private double UnboxValueToDouble(object fetchedValue, TypeCode typeCode)
