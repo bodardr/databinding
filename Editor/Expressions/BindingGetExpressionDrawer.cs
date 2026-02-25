4c6762a917c963024c28c334b1948de48381a45d
Romain Bodard <romain.bodard@videotron.ca>
2026-02-25 14:31:14 -0500

    Reverted GetExpressionDrawer

--- Editor/Expressions/BindingGetExpressionDrawer.cs
+++ Editor/Expressions/BindingGetExpressionDrawer.cs
@@ -39,34 +39,5 @@ namespace Bodardr.Databinding.Editor
                     (location, entries) => EditorDatabindingUtility.SetTargetPath(property, location, entries));
             }
         }
-        
-        public override VisualElement CreatePropertyGUI(SerializedProperty property)
-        {
-            var labelRect = new Rect(position);
-            labelRect.width = position.width - buttonWidth;
-
-            position.x += labelRect.width;
-            position.width -= labelRect.width;
-            var buttonRect = new Rect(position);
-
-            BindingInspectorCommon.DrawLabel("Source", property, labelRect);
-
-            if (GUI.Button(buttonRect, EditorGUIUtility.IconContent("editicon.sml")))
-            {
-                BindingExpressionPathValidator.TryFixingPath(property, (BindingGetExpression)property.boxedValue);
-                var searchCriteria = new BindingSearchCriteria(property);
-
-                var enumValues = Enum.GetValues(typeof(BindingExpressionLocation));
-                if (property.FindPropertyRelative("location").enumValueIndex ==
-                    Array.IndexOf(enumValues, BindingExpressionLocation.None))
-                    searchCriteria.Location = searchCriteria.BindingNode == null ? BindingExpressionLocation.Static
-                        : BindingExpressionLocation.InBindingNode;
-
-                searchCriteria.Flags = BindingSearchCriteria.PropertyFlag.Getter;
-
-                BindingSearchWindow.Open(searchCriteria,
-                    (location, entries) => EditorDatabindingUtility.SetTargetPath(property, location, entries));
-            }
-        }
     }
 }
